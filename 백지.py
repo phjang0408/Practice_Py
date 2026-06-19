6cd2a45cae04d6611ad344e5adcd9a8f0e74e474
phjang0408 <phjang0408@naver.com>
2026-06-19 22:31:19 +0900

    I'm done

--- 백지.py
+++ 백지.py
@@ -1,1 +1,12 @@
-print("Hello")
+a = [3,1,2]
+a.sort()
+
+b = ["banana", "apple", "kiwi"]
+b.sort(key = len)
+
+c = [-3, 1, -2]
+c.sort(key = abs)
+
+d = [(1, 3), (2, 1), (3, 2)]
+d.sort(key = lambda x : x[1])
+print(d)
